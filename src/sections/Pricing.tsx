741f77213ca0c4b2a825e592da034a1dc232f02c
Raul <raul_cosmin_26@yahoo.com>
2024-08-15 20:57:13 +0300

    Build the pricing section.

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,6 @@
+import CheckIcon from "@/assets/check.svg";
+import { twMerge } from "tailwind-merge";
+
 const pricingTiers = [
   {
     title: "Free",
@@ -52,13 +55,68 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
-        <h2 className="section-title mt-5">Pricing</h2>
-        <p>
-          Free forever. Upgrade for unlimited tasks, better security, and
-          exclusive features.
-        </p>
+        <div className="section-heading">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">
+            Free forever. Upgrade for unlimited tasks, better security, and
+            exclusive features.
+          </p>
+        </div>
+        <div className="mt-10 flex flex-col items-center gap-6 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map((tier, index) => (
+            <div
+              key={index}
+              className={twMerge(
+                "w-full max-w-xs rounded-3xl border border-[#F1F1F1] bg-white p-10 shadow-[0_7px_14px_#EAEAEA]",
+                tier.inverse === true && "border-black bg-black text-white",
+              )}
+            >
+              <div className="flex justify-between">
+                <h3
+                  className={twMerge(
+                    "text-lg font-bold text-black/50",
+                    tier.inverse === true && "text-white/60",
+                  )}
+                >
+                  {tier.title}
+                </h3>
+                {tier.popular === true && (
+                  <div className="inline-flex rounded-xl border border-white/20 px-4 py-1.5 text-sm">
+                    <span className="bg-[linear-gradient(to_right,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf)] bg-clip-text font-medium text-transparent">
+                      Popular
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className="mt-[30px] flex items-baseline gap-1">
+                <span className="text-4xl font-bold leading-none tracking-tighter">
+                  ${tier.monthlyPrice}
+                </span>
+                <span className="font-bold tracking-tight text-black/50">
+                  /month
+                </span>
+              </div>
+              <button
+                className={twMerge(
+                  "btn btn-primary mt-[30px] w-full",
+                  tier.inverse === true && "border-white bg-white text-black",
+                )}
+              >
+                {tier.buttonText}
+              </button>
+              <ul className="mt-8 flex flex-col gap-5">
+                {tier.features.map((feature, index) => (
+                  <li key={index} className="flex items-center gap-4 text-sm">
+                    <CheckIcon className="h-6 w-6" />
+                    <span>{feature}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,14 +7,14 @@ export const ProductShowcase = () => {
   return (
     <section className="overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
       <div className="container">
-        <div className="mx-auto max-w-[540px]">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Boost productivity</div>
           </div>
           <h2 className="section-title mt-5">
             A more effective way to track progress
           </h2>
-          <p className="mt-5 text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e]">
+          <p className="section-description mt-5">
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
