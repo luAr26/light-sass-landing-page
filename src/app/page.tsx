db62e154bf5263f6c327b2886c092da1fc9161cb
Raul <raul_cosmin_26@yahoo.com>
2024-08-15 14:41:36 +0300

    Build logo ticker.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,34 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-white py-8 md:py-12">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex flex-none gap-14">
+            <Image src={acmeLogo} className="logo-ticker-image" alt="Acme" />
+            <Image
+              src={quantumLogo}
+              className="logo-ticker-image"
+              alt="Quantum"
+            />
+            <Image src={echoLogo} className="logo-ticker-image" alt="Echo" />
+            <Image
+              src={celestialLogo}
+              className="logo-ticker-image"
+              alt="Celestial"
+            />
+            <Image src={pulseLogo} className="logo-ticker-image" alt="Pulse" />
+            <Image src={apexLogo} className="logo-ticker-image" alt="Apex" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
