76282416cae6a1a1b8216e44d08b6d467cdc7f68
Raul <raul_cosmin_26@yahoo.com>
2024-08-17 13:31:48 +0300

    Add parallax effect to product showcase section.

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,23 @@
+"use client";
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+    <section
+      ref={sectionRef}
+      className="overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24"
+    >
       <div className="container">
         <div className="section-heading">
           <div className="flex justify-center">
@@ -21,19 +33,21 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="Product" className="mt-10" />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="Pyramid"
             height={262}
             width={262}
             className="absolute -right-36 -top-32 hidden md:block"
+            style={{ translateY }}
           />
-          <Image
-            src={tubeImage}
+          <motion.img
+            src={tubeImage.src}
             alt="Tube"
             height={248}
             width={248}
             className="absolute -left-36 bottom-24 hidden md:block"
+            style={{ translateY }}
           />
         </div>
       </div>
